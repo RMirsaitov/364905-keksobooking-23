bd3535d73b66bbf85d3a0ad1872baa6e5e3f2d95
RMirsaitov <mirsaitov.ruslan@gmail.com>
2021-06-07 14:52:04 +0300

    Исправил ошибки в функциях

--- js/main.js
+++ js/main.js
@@ -1,17 +1,19 @@
 function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  const RandomNum = Math.floor(Math.random() * (max - min + 1)) + min;
-  return RandomNum;
+  min = Math.abs(Math.ceil(min));
+  max = Math.abs(Math.floor(max));
+  if (max <= min) {
+    throw new Error('Максимальное число не должно быть меньше или равно минимальному!');
+  }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 getRandomInt();
 
 function getRandomIntFloat(min, max, floatNumber) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  const RandomNumFloat = (Math.random() * (max - min + 1) + min).toFixed(
-    floatNumber
-  );
-  return RandomNumFloat;
+  min = Math.abs(Math.ceil(min));
+  max = Math.abs(Math.floor(max));
+  if (max <= min) {
+    throw new Error('Максимальное число не должно быть меньше или равно минимальному');
+  }
+  return (Math.random() * (max - min + 1) + min).toFixed(floatNumber);
 }
 getRandomIntFloat();
