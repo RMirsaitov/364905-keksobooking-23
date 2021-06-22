3b6fd1f7ac5abd05f94f3ff997a235547e89b2c6
RMirsaitov <mirsaitov.ruslan@gmail.com>
2021-06-22 03:04:03 +0300

    Внес мелкие правки

--- js/main.js
+++ js/main.js
@@ -5,7 +5,6 @@ const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'condit
 const PHOTOS = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
 const quantityAds= 10;
 
-
 // Генерируем число из заданного диапазона
 const getRandomNumber = (valueFrom, valueUpTo, digits = 0) => {
   const lower = Math.min(Math.abs(valueFrom), Math.abs(valueUpTo));
