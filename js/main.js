119425bd63ddaff942607f0da9c52094ebc6de81
RMirsaitov <mirsaitov.ruslan@gmail.com>
2021-06-22 03:28:50 +0300

    Исправил ошибку в функции

--- js/main.js
+++ js/main.js
@@ -4,21 +4,17 @@ const TIME = ['12:00', '13:00', '14:00'];
 const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 const PHOTOS = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
 const quantityAds= 10;
-
 // Генерируем число из заданного диапазона
 const getRandomNumber = (valueFrom, valueUpTo, digits = 0) => {
   const lower = Math.min(Math.abs(valueFrom), Math.abs(valueUpTo));
   const upper = Math.max(Math.abs(valueFrom), Math.abs(valueUpTo));
-  const randomResult = Math.random() * (upper - lower) + lower;
+  const randomResult = Math.random() * (upper - lower + 1) + lower;
   return randomResult.toFixed(digits);
 };
-
 // Получаем случайный елемента из массива
 const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
-
 // Получаем часть массива случайной длины
-const getArrayRandomSlice = (elements) => elements.slice(0, getRandomNumber(1, elements.length));
-
+const getRandomArraySlice = (elements) => elements.slice(0, getRandomNumber(1, elements.length));
 // Генерируем объявление
 const generateAd = () => {
   const coordinates = {
@@ -27,7 +23,6 @@ const generateAd = () => {
   };
   // Генерируем индекс для URL
   const idNumber = getRandomNumber(1, 10).padStart(2, 0);
-
   return {
     author: {
       avatar: `img/avatars/user${idNumber}.png`,
@@ -41,16 +36,14 @@ const generateAd = () => {
       guests: getRandomNumber(1, 10),
       checkin: getRandomArrayElement(TIME),
       checkout: getRandomArrayElement(TIME),
-      features: getArrayRandomSlice(FEATURES),
+      features: getRandomArraySlice(FEATURES),
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at orci augue. Duis tincidunt sed magna a hendrerit. Curabitur ac.',
-      photos: getArrayRandomSlice(PHOTOS),
+      photos: getRandomArraySlice(PHOTOS),
     },
     location: coordinates,
   };
 };
-
 // Генерируем массив из случайных объявлений
 const generateAdsArray = () => new Array(quantityAds).fill(null).map(() => generateAd());
-
 // eslint-disable-next-line no-console
 console.log(generateAdsArray());
